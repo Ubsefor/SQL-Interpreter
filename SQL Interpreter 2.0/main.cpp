d93c41b2030d2c5591c86da59186d9eb4d01494b
Ubsefor <letzdropthebas@gmail.com>
2020-05-22 15:57:57 +0300

    Initial Commit

---
+++ SQL Interpreter 2.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SQL Interpreter 2.0
+//
+//  Created by Rave Botovski on 22.05.2020.
+//  Copyright © 2020 Rave Botovski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
